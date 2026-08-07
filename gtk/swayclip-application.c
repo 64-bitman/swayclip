4b6a913970442e273192303e815e3a48707ca02c
Foxe Chen <chen.foxe@gmail.com>
2026-08-07 18:57:43 -0400

    window working

--- gtk/swayclip-application.c
+++ gtk/swayclip-application.c
@@ -29,8 +29,54 @@ struct _SwayclipApplication
 G_DEFINE_TYPE(SwayclipApplication, swayclip_application, GTK_TYPE_APPLICATION)
 
 static void
+swayclip_application_dispose(GObject *obj)
+{
+    SwayclipApplication *self = SWAYCLIP_APPLICATION(obj);
+
+    g_clear_object(&self->ct);
+
+    G_OBJECT_CLASS(swayclip_application_parent_class)->dispose(obj);
+}
+
+static void
+swayclip_application_activate(GApplication *app)
+{
+    SwayclipApplication *self = SWAYCLIP_APPLICATION(app);
+
+    GtkWindow *win =
+        GTK_WINDOW(gtk_application_window_new(GTK_APPLICATION(app)));
+
+    gtk_window_set_title(win, "swayclip");
+    gtk_window_set_default_size(win, 400, 300);
+
+    // Root GtkBox that contains everything
+    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
+
+    gtk_window_set_child(win, vbox);
+
+    gtk_window_present(win);
+}
+
+static void
+swayclip_application_startup(GApplication *app)
+{
+    SwayclipApplication *self = SWAYCLIP_APPLICATION(app);
+
+    self->ct = swayclip_connection_new();
+
+    G_APPLICATION_CLASS(swayclip_application_parent_class)->startup(app);
+}
+
+static void
 swayclip_application_class_init(SwayclipApplicationClass *class)
 {
+    GObjectClass      *obj_class = G_OBJECT_CLASS(class);
+    GApplicationClass *app_class = G_APPLICATION_CLASS(class);
+
+    obj_class->dispose = swayclip_application_dispose;
+
+    app_class->startup = swayclip_application_startup;
+    app_class->activate = swayclip_application_activate;
 }
 
 static void
