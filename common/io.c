1bc69d1427e3988de13fd522c25e847371ceb68e
Foxe Chen <chen.foxe@gmail.com>
2026-08-07 01:06:24 -0400

    remove unused function

--- common/io.c
+++ common/io.c
@@ -131,67 +131,6 @@ fail:
     return false;
 }
 
-/*
- * Write to the context with timeout in milliseconds. "data_callback" should
- * return true and set "*len" to zero if finished.
- */
-bool
-io_write(struct io_write *ctx, int timeout)
-{
-    uint8_t *ptr = ctx->buf;
-    size_t   len = 0;
-
-    struct pollfd pfd = {.fd = ctx->fd, .events = POLLOUT};
-
-    while (true)
-    {
-        int ret = poll(&pfd, 1, timeout);
-
-        if (ret <= 0)
-        {
-            if (ret == -1)
-            {
-                if (errno == EINTR)
-                    continue;
-                log_errerror("Error polling fd %d", ctx->fd);
-            }
-            else
-                log_errerror("Timed out writing to fd %d", ctx->fd);
-            return false;
-        }
-        if (!(pfd.revents & POLLOUT) &&
-            pfd.revents & (POLLHUP | POLLERR | POLLNVAL))
-            return false;
-        else if (!(pfd.revents & POLLOUT))
-            continue;
-
-        if (len == 0)
-        {
-            ptr = ctx->buf;
-            if (!ctx->data_callback(
-                    ptr, ctx->bufsize, &len, ctx->callback_udata
-                ))
-                return false;
-            if (len == 0)
-                return true;
-        }
-
-        ssize_t w = write(ctx->fd, ptr, len);
-
-        if (w <= 0)
-        {
-            if (w == -1 &&
-                (errno == EINTR || errno == EAGAIN || errno == EWOULDBLOCK))
-                continue;
-            log_errerror("Error writing data");
-            return false;
-        }
-
-        ptr += w;
-        len -= w;
-    }
-    return true;
-}
 
 /*
  * Receive "len" bytes from "fd" into "buf", handling EINTR. If EAGAIN or
